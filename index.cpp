1d8c63fefffdc4cb0b119b420faa57f83980f7d6
Ubinquitous <ubinquitous1@gmail.com>
2022-11-30 22:09:03 +0900

    ADD : 기본 메뉴, 선택값 처리 기본값

---
+++ index.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int select;
+
+/* define */
+
+/* variable define*/
+
+/* function */
+
+/* main function */
+int main()
+{
+    printf("Nabin Market ───────\n\n");
+
+    printf(" * 나빈 마켓에 오신 것을 환영합니다. \n");
+    printf(" * 나빈 마켓은 C++로 구현한 매장 물품 관리 프로그램입니다. \n\n");
+
+    printf("나빈 마켓 현황 ─────\n\n");
+
+    printf(" * 나빈 마켓 현재 물품 개수 (데이터 개수) : %d\n", 17);
+    printf(" * 나빈 마켓 현재 진열 공간 (메모리 크기) : %d\n", 17 * 4);
+
+    printf("나빈 마켓 메뉴 ─────\n\n");
+
+    printf(" * 물품 목록 조회 ( 1을 누르세요 )\n");
+    printf(" * 마켓 물품 추가 ( 2를 누르세요 )\n");
+    printf(" * 마켓 물품 삭제 ( 3을 누르세요 )\n");
+    printf(" * 물품 찜 & 구매 ( 4를 누르세요 )\n\n");
+    printf("메뉴를 선택하세요 : ");
+    scanf("%d", &select);
+
+    if (select == 1)
+    {
+        /* showAllItem(); */
+    }
+    else if (select == 2)
+    {
+        /* insertItem(); */
+    }
+    else if (select == 3)
+    {
+        /* deleteItem(); */
+    }
+    else if (select == 4)
+    {
+        /* selectItem(); */
+    }
+}
